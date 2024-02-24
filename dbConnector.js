558ad7613e78120dd8e236b4c8683b61f8dc28c0
OkeanFeodor <151923198+OkeanFeodor@users.noreply.github.com>
2024-02-24 19:27:55 +0300

    Sdelal connector polnostyu

--- dbConnector.js
+++ dbConnector.js
@@ -17,7 +17,48 @@ const pool = mysql.createPool({
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise()
+//--------------------------------------------------------
+
+export async function getUsers() {
+    const [rows] = await pool.query("SELECT * FROM users")
+    return rows
+    //console.log(rows)
+
+}
+
+export async function getUser(id) {
+    const [rows] = await pool.query(`
+    SELECT *
+    FROM users
+    WHERE id = ?
+    `, [id])
+    return rows[0]
+}
+
+export async function createUser(user, passwordd) {
+   const [result] = await pool.query(`
+    INSERT INTO users (user, passwordd)
+    VALUES (?, ?)
+    `, [user, passwordd])
+    //return result.insertId
+    const id = result.insertId
+    return getUser(id) 
+}
+
+//##############################################
+
+const result = await createUser('admin6', 'admin')
+console.log(result)
+
+/*
+
+const user = await getUser(2)
+console.log(user)
+*/
 
 
-const result = await pool.query("SELECT * FROM users")
-console.log(result[0])
+/*
+const users = await getUsers()
+console.log(users)
+
+*/
